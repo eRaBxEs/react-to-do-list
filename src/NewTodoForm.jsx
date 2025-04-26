9376a7073c262aaa97e5d4986c47cd1873536c1a
Ehioje Henry Erabor <ehiojee@yahoo.com>
2025-04-26 17:49:07 +0100

    destructuring before using a props

--- src/NewTodoForm.jsx
+++ src/NewTodoForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export function NewTodoForm(props) {
+export function NewTodoForm({ onSubmit }) {
     const [newItem, setNewItem] = useState("")
 
     function handleSubmit(e) {
@@ -8,7 +8,7 @@ export function NewTodoForm(props) {
 
         if (newItem === "") return
 
-        props.onSubmit(newItem)
+        onSubmit(newItem)
 
         setNewItem("")
     }
